7cf2022b83afaa27c36bce54934e4a28417ec8dc
mgates7 <35174812+mgates7@users.noreply.github.com>
2018-09-22 01:42:08 -0400

    Works for all...except evil

--- main.cpp
+++ main.cpp
@@ -20,8 +20,13 @@ void copyV(d_vec v1, d_vec& v2); // v2 becomes copy of v1
 void printV(d_vec v, bool pass);
 void eraseVal(vec& v, int n);
 void simpleTest(d_vec& v);
-void test1(d_vec& v);
 bool solve(d_vec v);
+
+void test1(d_vec& v);
+void test2(d_vec& v);
+void test3(d_vec& v);
+void test4(d_vec& v);
+void test5(d_vec& v);
 
 int main() {
 
@@ -32,7 +37,7 @@ int main() {
     int testNum = 0;
 
     // Read from text file
-    ifstream infile("medium.txt");
+    ifstream infile("evil1.txt");
     for (int i = 0; i < 9; i++) {
         for (int j = 0; j < 9; j++) {
             infile >> puzzle[i][j];
@@ -47,19 +52,48 @@ int main() {
                 simpleTest(temp);
                 pass = solve(temp);
                 printV(temp, pass);
+                if (pass)
+                    return 0;
             case 1:
                 cout << "\nRunning test1..." << endl;
                 test1(temp);
                 pass = solve(temp);
                 printV(temp, pass);
+                if (pass)
+                    return 0;
+            case 2:
+                cout << "\nRunning test2..." << endl;
+                test2(temp);
+                pass = solve(temp);
+                printV(temp, pass);
+                if (pass)
+                    return 0;
+            case 3:
+                cout << "\nRunning test3..." << endl;
+                test3(temp);
+                pass = solve(temp);
+                printV(temp, pass);
+                if (pass)
+                    return 0;
+            case 4:
+                cout << "\nRunning test4..." << endl;
+                test4(temp);
+                pass = solve(temp);
+                printV(temp, pass);
+                if (pass)
+                    return 0;
+            case 5:
+                cout << "\nRunning test5..." << endl;
+                test5(temp);
+                pass = solve(temp);
+                printV(temp, pass);
+                if (pass)
+                    return 0;
             default:
                 return 0;
         }
         testNum++;
     }
-
-
-
     return 0;
 }
 
@@ -148,7 +182,7 @@ end:
     }
 }
 
-// Brute force
+// Brute force with trying all 9 combs for each 0
 
 void test1(d_vec& v) {
 
@@ -172,45 +206,236 @@ void test1(d_vec& v) {
             }
         }
     }
+}
+
+// More advanced brute force
+
+void test2(d_vec& v) {
+    vec testV(9);
+    testV = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    d_vec tempDV = v; // make copy
+    bool pass = 0;
+    vector<int*> zeroPoints; // location of zeros
+
+    for (int i = 0; i < v.size(); i++)
+        for (int j = 0; j < v.size(); j++)
+            if (v[i][j] == 0)
+                zeroPoints.push_back(&v[i][j]);
+
+    for (int i = 0; i < zeroPoints.size(); i++) {
+        for (int j = i + 1; j < zeroPoints.size(); j++) {
+            for (int ii = 0; ii < 9; ii++) {
+                for (int jj = 0; jj < 9; jj++) {
+                    //cout << i << j << endl;
+                    *zeroPoints[i] = testV[ii];
+                    *zeroPoints[j] = testV[jj];
+                    copyV(v, tempDV);
+                    simpleTest(tempDV);
+                    pass = solve(tempDV);
+                    if (pass) {
+                        copyV(tempDV, v);
+                        return;
+                    }
+                }
+            }
+            // reset back to zero
+            *zeroPoints[i] = 0;
+            *zeroPoints[j] = 0;
+        }
+    }
+}
 
 
+// Even more advanced brute force
+
+void test3(d_vec& v) {
+    vec testV(9);
+    testV = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    d_vec tempDV = v; // make copy
+    bool pass = 0;
+    vector<int*> zeroPoints; // location of zeros
+
+    for (int i = 0; i < v.size(); i++)
+        for (int j = 0; j < v.size(); j++)
+            if (v[i][j] == 0)
+                zeroPoints.push_back(&v[i][j]);
+
+    for (int i = 0; i < zeroPoints.size(); i++) {
+        for (int j = i + 1; j < zeroPoints.size(); j++) {
+            for (int k = j + 1; k < zeroPoints.size(); k++) {
+                for (int ii = 0; ii < 9; ii++) {
+                    for (int jj = 0; jj < 9; jj++) {
+                        for (int kk = 0; kk < 9; kk++) {
+                            //cout << i << j << endl;
+                            *zeroPoints[i] = testV[ii];
+                            *zeroPoints[j] = testV[jj];
+                            *zeroPoints[k] = testV[kk];
+                            copyV(v, tempDV);
+                            simpleTest(tempDV);
+                            pass = solve(tempDV);
+                            if (pass) {
+                                copyV(tempDV, v);
+                                return;
+                            }
+                        }
+                    }
+                }
+                // reset back to zero
+                *zeroPoints[i] = 0;
+                *zeroPoints[j] = 0;
+                *zeroPoints[k] = 0;
+            }
+        }
+    }
+}
+
+// There must be a better way...
+
+void test4(d_vec& v) {
+    vec testV(9);
+    testV = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    d_vec tempDV = v; // make copy
+    bool pass = 0;
+    vector<int*> zeroPoints; // location of zeros
+
+    for (int i = 0; i < v.size(); i++)
+        for (int j = 0; j < v.size(); j++)
+            if (v[i][j] == 0)
+                zeroPoints.push_back(&v[i][j]);
+
+    for (int i = 0; i < zeroPoints.size(); i++) {
+        for (int j = i + 1; j < zeroPoints.size(); j++) {
+            for (int k = j + 1; k < zeroPoints.size(); k++) {
+                for (int l = k + 1; l < zeroPoints.size(); l++) {
+                    for (int ii = 0; ii < 9; ii++) {
+                        for (int jj = 0; jj < 9; jj++) {
+                            for (int kk = 0; kk < 9; kk++) {
+                                for (int ll = 0; ll < 9; ll++) {
+                                    //cout << i << j << endl;
+                                    *zeroPoints[i] = testV[ii];
+                                    *zeroPoints[j] = testV[jj];
+                                    *zeroPoints[k] = testV[kk];
+                                    *zeroPoints[l] = testV[ll];
+                                    copyV(v, tempDV);
+                                    simpleTest(tempDV);
+                                    pass = solve(tempDV);
+                                    if (pass) {
+                                        copyV(tempDV, v);
+                                        return;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    // reset back to zero
+                    *zeroPoints[i] = 0;
+                    *zeroPoints[j] = 0;
+                    *zeroPoints[k] = 0;
+                    *zeroPoints[l] = 0;
+                }
+            }
+        }
+    }
+}
+
+// There must be......a....better....way...hmmm
+
+void test5(d_vec& v) {
+    vec testV(9);
+    testV = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    d_vec tempDV = v; // make copy
+    bool pass = 0;
+    vector<int*> zeroPoints; // location of zeros
+
+    for (int i = 0; i < v.size(); i++)
+        for (int j = 0; j < v.size(); j++)
+            if (v[i][j] == 0)
+                zeroPoints.push_back(&v[i][j]);
+
+    for (int i = 0; i < zeroPoints.size(); i++) {
+        for (int j = i + 1; j < zeroPoints.size(); j++) {
+            for (int k = j + 1; k < zeroPoints.size(); k++) {
+                for (int l = k + 1; l < zeroPoints.size(); l++) {
+                    for (int m = l + 1; m < zeroPoints.size(); m++) {
+                        for (int ii = 0; ii < 9; ii++) {
+                            for (int jj = 0; jj < 9; jj++) {
+                                for (int kk = 0; kk < 9; kk++) {
+                                    for (int ll = 0; ll < 9; ll++) {
+                                        for (int mm = 0; mm < 9; mm++) {
+                                            //cout << i << j << endl;
+                                            *zeroPoints[i] = testV[ii];
+                                            *zeroPoints[j] = testV[jj];
+                                            *zeroPoints[k] = testV[kk];
+                                            *zeroPoints[l] = testV[ll];
+                                            *zeroPoints[m] = testV[mm];
+                                            copyV(v, tempDV);
+                                            simpleTest(tempDV);
+                                            pass = solve(tempDV);
+                                            if (pass) {
+                                                copyV(tempDV, v);
+                                                return;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        // reset back to zero
+                        *zeroPoints[i] = 0;
+                        *zeroPoints[j] = 0;
+                        *zeroPoints[k] = 0;
+                        *zeroPoints[l] = 0;
+                        *zeroPoints[m] = 0;
+                    }
+                }
+            }
+        }
+    }
 }
 
 // Tests for solution
 
 bool solve(d_vec v) {
-    int sum = 0;
+    vec testV(9);
+    testV = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
     // Test each row
     for (int i = 0; i < v.size(); i++) {
-        sum = 0;
+        testV = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         for (int j = 0; j < v.size(); j++) {
-            sum += v[i][j];
+            if (v[i][j] == 0)
+                return 0;
+            eraseVal(testV, v[i][j]);
         }
-        if (sum != 45)
+        if (testV.size() != 0)
             return 0;
     }
 
     // Test each column
+    testV = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     for (int i = 0; i < v.size(); i++) {
-        sum = 0;
+        testV = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         for (int j = 0; j < v.size(); j++) {
-            sum += v[j][i];
+            if (v[j][i] == 0)
+                return 0;
+            eraseVal(testV, v[j][i]);
         }
-        if (sum != 45)
+        if (testV.size() != 0)
             return 0;
     }
 
     // Test each sub-matrix
+    testV = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     for (int subX = 0; subX < 2; subX++) {
         for (int subY = 0; subY < 2; subY++) {
-            sum = 0;
+            testV = {1, 2, 3, 4, 5, 6, 7, 8, 9};
             for (int ii = subX * 3; ii < subX * 3 + 3; ii++) {
                 for (int jj = subY * 3; jj < subY * 3 + 3; jj++) {
-                    sum += v[ii][jj];
+                    eraseVal(testV, v[ii][jj]);
                 }
             }
-            if (sum != 45)
+
+            if (testV.size() != 0)
                 return 0;
         }
     }
